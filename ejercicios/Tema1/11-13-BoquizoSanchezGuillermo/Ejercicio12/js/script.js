0c102d9484beda8488f2af27da9883add74550ac
“gboquizo” <guillermoboquizosanchez@gmail.com>
2018-10-02 23:48:29 +0200

    making some changes in script.js

--- ejercicios/Tema1/11-13-BoquizoSanchezGuillermo/Ejercicio12/js/script.js
+++ ejercicios/Tema1/11-13-BoquizoSanchezGuillermo/Ejercicio12/js/script.js
@@ -5,30 +5,45 @@ Completar el código JavaScript proporcionado para que cuando se pinche sobre el
 Autor: Guillermo Boquizo Sánchez
 */
 {
-    let more;
-    let linkReadMore;
+    /*  let more;
+     let linkReadMore;
 
-    function showMeMore() {
-        more.style.display = "none";
-        linkReadMore.addEventListener("click", function (event) {
+     function showMeMore() {
+         more.style.display = "none";
+         linkReadMore.addEventListener("click", function (event) {
 
+             event.preventDefault;
+
+             more.style.display = "inline";
+
+             this.style.display = "none";
+
+         });
+     }
+
+     document.addEventListener('DOMContentLoaded', init);
+
+     function init() {
+
+         more = document.getElementById("more");
+
+         linkReadMore = document.getElementById("linkReadMore");
+
+         showMeMore();
+     } */
+
+    {
+        let more = document.getElementById("more");
+
+        document.getElementById("linkReadMore").addEventListener("click", function (event) {
             event.preventDefault;
-
             more.style.display = "inline";
-
             this.style.display = "none";
+        });
 
+        document.addEventListener('DOMContentLoaded', function () {
+            more.style.display = "none"
         });
     }
 
-    document.addEventListener('DOMContentLoaded', init);
-
-    function init() {
-
-        more = document.getElementById("more");
-
-        linkReadMore = document.getElementById("linkReadMore");
-
-        showMeMore();
-    }
 }
