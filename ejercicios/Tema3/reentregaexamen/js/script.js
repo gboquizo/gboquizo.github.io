81f20adb809a9215ec91c489a84f05e392b3c9e4
Guillermo <guillermoboquizosanchez@gmail.com>
2018-11-21 09:55:34 +0100

    fixing routes

--- ejercicios/Tema3/reentregaexamen/js/script.js
+++ ejercicios/Tema3/reentregaexamen/js/script.js
@@ -12,12 +12,12 @@
 
     let abrirContador = function (ev) {
         ev.preventDefault;
-        window.open("./contador.html", "_self");
+        window.open("contador.html", "_self");
     }
 
     let abrirNombres = function (ev) {
         ev.preventDefault;
-        window.open("./nombres.html", "_self");
+        window.open("nombres.html", "_self");
     }
 
     document.addEventListener('DOMContentLoaded', init);
