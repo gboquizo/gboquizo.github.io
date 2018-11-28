1e9114fe09fe58a694d53086c3255457137b3eaf
Guillermo <guillermoboquizosanchez@gmail.com>
2018-11-28 10:14:07 +0100

    creating lindogatito, addying meta to examen custom

---
+++ ejercicios/Tema4/lindogatito/js/main.js
@@ -0,0 +1,43 @@
+{
+
+    let boton;
+
+    function init() {
+        boton = document.getElementById("crearGato");
+        boton.addEventListener("clic", crearGato);
+
+    }
+
+    let crearGato = () => {
+        let auryn = new Gato('Auryn', '01/01/2004', 'Siamesa', '4');
+        let ventana = window.open("", "", "");
+        ventana.innerHTML = `
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <meta name="author" content="Guillermo Boquizo Sánchez">
+            <meta name="description" content="Crea un gato auryn en js">
+            <meta name="keywords" content="HTML,CSS,JavaScript">
+            <title>Gata auryn</title>
+            <link rel="stylesheet" href="css/estilos.css">
+            <script src="js/gato.js"></script>
+            <script src="js/main.js"></script>
+        </head>
+        <body>
+            <noscript>
+                <p>Por favor, compruebe que su navegador sea compatible con javascript,
+                    o bien compruebe si lo tiene activado.</p>
+            </noscript>
+            <main>
+                <h1>Lindo gatito js</h1>
+                <p>${auryn.nombre}</p>
+            </main>
+        </body>
+        </html>    
+        `;
+    }
+    document.addEventListener('DOMContentLoaded', init);
+}
