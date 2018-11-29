9fe18764c1003ae3467dea3800f237c6cb69f904
Guillermo Boquizo Sánchez <guillermoboquizosanchez@gmail.com>
2018-11-29 17:29:50 +0100

    creating lindogatito main structure

--- ejercicios/Tema4/lindogatito/js/gato.js
+++ ejercicios/Tema4/lindogatito/js/gato.js
@@ -1,7 +1,10 @@
 /**
  * 
  *  Crea una clase Gato, y a partir de ella crea tantos gatos como quiera el usuario.
- *  Cada gato tendrá un nombre, una fecha de nacimiento, una raza y un peso(1 - 15).Cada vez que crees un objeto gato aparecerá una ventana nueva con una imagen que cambiará en función de su estado(comiendo, durmiendo y jugando, que es su estado habitual).El usuario podrá averiguar la edad del gato partiendo de un evento.
+ *  Cada gato tendrá un nombre, una fecha de nacimiento, una raza y un peso(1 - 15).
+ *  Cada vez que crees un objeto gato aparecerá una ventana nueva con una imagen que cambiará 
+ *  en función de su estado(comiendo, durmiendo y jugando, que es su estado habitual).
+ *  El usuario podrá averiguar la edad del gato partiendo de un evento.
  *  El comportamiento es el siguiente:
  *
  *  Cuando el gato juega pierde peso
@@ -19,32 +22,90 @@
  *
  */
 {
-    function Gato(nombre, fecha, raza, peso) {
+    function Gato(nombre, fechaNacimiento, raza) {
         this.nombre = nombre;
-        this.fecha = fecha;
+        this.fechaNacimiento = fechaNacimiento;
         this.raza = raza;
-        this.peso = peso;
+        this.peso = obtenerPeso(this.raza);
+        this.edad = calcularEdad(this.fechaNacimiento);
+
     }
 
-
-    Gato.prototype.estado = true;
+    Gato.prototype.estado = "jugando";
 
     Gato.prototype.jugar = function () {
-        if (this.peso > 1)
-            this.peso--;
-        this.estado = false;
+        this.peso -= 1;
+        this.estado = "jugando";
+        this.comprobarPesoValido();
     }
 
     Gato.prototype.comer = function () {
-        if (this.peso < 15)
-            this.peso++;
-        this.estado = false;
+        this.peso += 1;
+        this.estado = "comiendo";
+        this.comprobarPesoValido();
     }
 
-    Gato.prototype.obtenerEdad = function () {
-        let fechaActual = Date.now;
-        let fechaNacimieto = Date.parse(this.fecha);
-        let edad = fechaActual.getFullYear() - fechaNacimieto.getFullYear();
-        return edad;
+    Gato.prototype.dormir = function () {
+        this.estado = "durmiendo";
     }
+    Gato.prototype.comprobarPesoValido = function () {
+        if (this.peso < 1 || this.peso > 15) {
+            this.estado = "muerto";
+        }
+    }
+
+    let calcularEdad = function (fecha) {
+        let fechaActual = Date.now;
+        if (!esFecha(fecha) || fecha > fechaActual)
+            throw new FechaException("No es una fecha.");
+
+        let year = fechaActual.getFullYear - fecha.getFullYear();
+        let month = fechaActual.getMonth() - fecha.getMonth();
+        let day = fechaActual.getDay() - fecha.getDay();
+
+        if (year === 0 && month === 0) {
+            return day + " días";
+        } else if (year === 0 && month !== 0) {
+            return month + " meses y " + day + " días";
+        } else {
+            return year + " años, " + month + " meses y " + day + " días";
+        }
+    }
+
+    let esFecha = function (fecha) {
+        return !isNaN(Date.parse(fecha));
+    }
+
+    obtenerPeso = function (raza) {
+        let peso = 0;
+        switch (raza) {
+            case "savannah":
+                peso = 4;
+                break;
+            case "azul ruso":
+                peso = 7;
+                break;
+            case "maine coon":
+                peso = 5;
+                break;
+            case "ragdoll":
+                peso = 4;
+                break;
+            case "abisinio":
+                peso = 6;
+                break;
+            default:
+                peso = 3;
+                break;
+        }
+        return peso;
+    }
+
+    let generarVentana = function (object) {
+        let html = "";
+
+    }
+
+
+
 }
--- ejercicios/Tema4/lindogatito/js/main.js
+++ ejercicios/Tema4/lindogatito/js/main.js
@@ -1,18 +1,109 @@
 {
-
+    /**
+     * 
+     * 
+     * @author Guillermo Boquizo Sánchez
+     */
     let boton;
 
     function init() {
+        createPage();
         boton = document.getElementById("crearGato");
-        boton.addEventListener("click", crearGato);
-
+        boton.addEventListener("click", crearVentana);
     }
+    /**
+     * Función que crea el layout de la página desde js.
+     */
+    let createPage = function () {
 
-    let crearGato = () => {
-        let auryn = new Gato('Auryn', '01/01/2004', 'Siamesa', '4');
-        let ventana = window.open("", "", "");
-        document.open(ventana);
-        ventana.innerHTML = `
+        /**
+         * Sección main
+         */
+        //Crea la sección main y se le asigna una clase.
+        let main = document.createElement('main');
+        main.className = 'main';
+
+        //Crea un contenedor para el ejercicio y se le asigna una clase.
+        let container = document.createElement('container');
+        container.className = 'container';
+
+        //Crea un contenedor para el div y se le asigna una clase
+        let exercise = document.createElement('ejercicio');
+        exercise.className = 'ejercicio';
+
+        //Crea un fragment donde insertar la información del ejercicio.
+        let fragment = document.createDocumentFragment();
+        createExercise(fragment);
+        //Se añade el fragment al div del ejercicio.
+        exercise.appendChild(fragment);
+
+
+        //Se añade el contenedor del botón al container principal.
+        container.appendChild(exercise);
+
+        //Se añade el elemento container al main.
+        main.appendChild(container);
+        /**
+         * Sección footer
+         */
+
+        //Crea un footer y se le asigna una clase.
+        let footer = document.createElement('footer');
+        footer.className = 'footer';
+
+        //Crea un elemento p para el copyright y se le asigna un texto.
+        let p = document.createElement('p');
+        p.className = 'footer-copyright';
+        p.textContent = 'Guillermo Boquizo Sánchez - DWECL 2 º DAW IES Gran Capitán';
+
+        //Se añade el elemento p al footer.
+        footer.appendChild(p);
+
+        /**
+         * Sección document
+         */
+        //Se añade el main al body.
+        document.body.appendChild(main);
+        document.body.appendChild(footer);
+
+
+    };
+
+
+    let createExercise = function (fragment) {
+        let h2 = document.createElement('h2');
+        h2.textContent = "Creación de un lindo gatito."
+        let img = document.createElement('img');
+        img.src = "/ejercicios/Tema4/lindogatito/images/lindo.png"
+        boton = document.createElement('button');
+        boton.id = "crearGato";
+        boton.className = "btn";
+        boton.innerHTML = 'Crear Gato';
+        let descripcion = document.createElement("article");
+        descripcion.innerHTML = `
+        <h4 class="condiciones">Condiciones de creación</h4>
+        <ol>
+            <li class="descripcion">Un gato no puede pesar menos de 1 kg</li>
+            <li class="descripcion">Un gato no puede pesar más de 15 kg</li>
+            <li class="descripcion">Comer aumenta el peso del gato en 1 kg</li>
+            <li class="descripcion">Jugar disminuye el peso del gato en 1 kg</li>
+            <li class="descripcion">Si el gato no cumple su peso, muere</li>
+        </ol>
+        `;
+        fragment.appendChild(h2);
+        fragment.appendChild(img);
+        fragment.appendChild(descripcion);
+        fragment.appendChild(boton);
+    };
+
+
+    /*  let crearGato = () => {
+         let auryn = new Gato('Auryn', '01/01/2004', 'Siamesa', '4');
+     } */
+
+    let crearVentana = function (object) {
+        let ventana;
+        let html = `
         <!DOCTYPE html>
         <html lang="es">
         <head>
@@ -22,25 +113,37 @@
             <meta name="author" content="Guillermo Boquizo Sánchez">
             <meta name="description" content="Crea un gato auryn en js">
             <meta name="keywords" content="HTML,CSS,JavaScript">
-            <title>Gata auryn</title>
+            <title>Nuevo gato</title>
             <link rel="stylesheet" href="css/estilos.css">
-            <script src="js/gato.js"></script>
-            <script src="js/main.js"></script>
+            <script type="text/javascript" src="js/gato.js"></script>
         </head>
         <body>
             <noscript>
                 <p>Por favor, compruebe que su navegador sea compatible con javascript,
                     o bien compruebe si lo tiene activado.</p>
             </noscript>
-            <main>
-                <h1>Lindo gatito js</h1>
-                <p>${auryn.nombre}</p>
+            <header class="header">
+                <h1>Desarrollo Web en Entorno Cliente</h1>
+            </header>
+            <main class="main">
+            <container class="container">
+            <ejercicio class="ejercicio">
+                <h2>Nuevo gatito js</h2>
+                <p>Prueba</p>
+            </ejercicio>
+            </container>
             </main>
+            <footer class="footer">
+                <p class="footer-copyright">Guillermo Boquizo Sánchez - DWECL 2 º DAW IES Gran Capitán</p>
+            </footer>
         </body>
         </html>    
         `;
-        document.write();
-        document.close();
+        ventana = window.open('', '_self');
+        ventana.document.open();
+        ventana.document.gato = object;
+        ventana.document.write(html);
+        ventana.document.close();
     }
     document.addEventListener('DOMContentLoaded', init);
 }
