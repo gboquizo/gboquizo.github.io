6229bf19eeb5ee843c78cb7f25a5158bebc52d03
Guillermo <guillermoboquizosanchez@gmail.com>
2019-01-17 22:43:01 +0100

    addying part VII

--- ejercicios/Tema7/codigos-manual-jquery-VII-VIII/parteVII/js/demo2.js
+++ ejercicios/Tema7/codigos-manual-jquery-VII-VIII/parteVII/js/demo2.js
@@ -3,6 +3,33 @@
  * @author Guillermo Boquizo Sánchez
  */
 {
+    //creo el plugin cuentaCaracteres
+    jQuery.fn.cuentaCaracteres = function () {
+        //para cada uno de los elementos del objeto jQuery
+        this.each(function () {
+            //creo una variable elem con el elemento actual, suponemos un textarea
+            elem = $(this);
+            //creo un elemento DIV sobre la marcha 
+            let contador = $('<div>Contador caracteres: ' + elem.val().length + '</div>');
+            //inserto el DIV después del elemento textarea
+            elem.after(contador);
+            //guardo una referencia al elemento DIV en los datos del objeto jQuery
+            elem.data("campocontador", contador);
+
+            //creo un evento keyup para este elemento actual
+            elem.keyup(function () {
+                //creo una variable elem con el elemento actual, suponemos un textarea
+                let elem = $(this);
+                //recupero el objeto que tiene el elemento DIV contador asociado al textarea
+                let campocontador = elem.data("campocontador");
+                //modifico el texto del contador, para actualizarlo con el número de caracteres escritos
+                campocontador.text('Contador caracteres: ' + elem.val().length);
+            });
+        });
+        //siempre tengo que devolver this
+        return this;
+    };
+
     let init = function () {
         $("textarea").cuentaCaracteres();
     };
