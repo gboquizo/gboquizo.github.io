0d566a36576a026249708fe3668877fb3289dbc0
Guillermo <guillermoboquizosanchez@gmail.com>
2019-02-13 23:55:21 +0100

    preparing ajax III exercise

---
+++ ejercicios/Tema8/AJAX-jQuery-III/info.php
@@ -0,0 +1,36 @@
+<?php
+
+header('Content-type: text/plain; charset=utf-8');
+
+$students = [
+    ["first_name" => "José Rafael", "last_name" => "Álvarez Espino"],
+    ["first_name" => "Angelo", "last_name" => "Barbara"],
+    ["first_name" => "Guillermo", "last_name" => "Boquizo Sánchez"],
+    ["first_name" => "José Manuel", "last_name" => "Bravo Martínez"],
+    ["first_name" => "Juan Antonio", "last_name" => "Bujalance Martínez"],
+    ["first_name" => "Javier", "last_name" => "Frías Serrano"],
+    ["first_name" => "Marcos", "last_name" => "Gallardo Pérez"],
+    ["first_name" => "Rafael", "last_name" => "García Zurita"],
+    ["first_name" => "Javier", "last_name" => "González Guzmán"],
+    ["first_name" => "Samuel", "last_name" => "Luque Reyes"],
+    ["first_name" => "Jesús", "last_name" => "Mejías Leiva"],
+    ["first_name" => "Adrián", "last_name" => "Moya Moruno"],
+    ["first_name" => "Mario", "last_name" => "Navarro Madrid"],
+    ["first_name" => "Francisco", "last_name" => "Ramírez Ruíz"],
+    ["first_name" => "José María", "last_name" => "Romero Ruíz"],
+    ["first_name" => "Rafael", "last_name" => "Sojo Ruíz"],
+];
+$searchs = array();
+?>
+<?php if ($_GET["search"]): ?>
+    <?php foreach ($students as $key => $student): ?>
+        <?php if (preg_match('/' . $_GET["search"] . '/i', $student['first_name'] . " " . $student['last_name'])):
+    array_push($searchs, $student);?>
+				<?php endif?>
+    <?php endforeach?>
+    <?php if (sizeof($searchs) === 0): ?>
+        <?php echo "No se encontraron resultados" ?>
+    <?php else: ?>
+        <?php echo json_encode($searchs, JSON_UNESCAPED_UNICODE) ?>
+    <?php endif?>
+<?php endif?>
---
+++ ejercicios/Tema8/AJAX-jQuery-III/js/main.js
@@ -0,0 +1,40 @@
+{
+	let init = function() {
+		$('#search').keyup(function() {
+			let value = $(this).val() !== '' ? $(this).val() : '';
+
+			if (value.length >= 0) {
+			}
+			$.ajax({
+				type: 'GET',
+				url: './info.php',
+				data: {
+					search: value
+				},
+				dataType: 'text',
+				success: function(results) {
+					let resultsJson =
+						results !== '' && results !== 'No se encontraron resultados' ? JSON.parse(results) : {};
+					if (!$.isEmptyObject(resultsJson)) {
+						$('#showSearch').html('');
+						let html = ``;
+						for (let i in resultsJson) {
+							html += `<p class="suggest">${resultsJson[i].first_name} ${resultsJson[i].last_name}</p>`;
+						}
+						$('#showSearch').append(html);
+						$('.suggest').click(function() {
+							$('#search').val($(this).text());
+						});
+					} else {
+						$('#showSearch').html(`<p class="animated shake">No se encontraron resultados</p>`);
+					}
+				},
+				complete: function() {
+					$('#showSearch').css('background', 'rgba(255,255,255,0.2)');
+				}
+			});
+		});
+	};
+
+	$(init);
+}
