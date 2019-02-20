9e5f473bd2ce2cf942168ffcbc8fd6f5a4b55e5f
Guillermo <guillermoboquizosanchez@gmail.com>
2019-02-20 21:28:19 +0100

    fixing

--- ejercicios/Tema8/AJAX-jQuery-II/js/main.js
+++ ejercicios/Tema8/AJAX-jQuery-II/js/main.js
@@ -13,76 +13,73 @@
  * @author Guillermo Boquizo Sánchez
  */
 {
+	let init = function() {
+		$('button').click(function() {
+			showJSON('./json/' + $(this).attr('json'));
+			$('button').removeClass('active');
+			$(this).addClass('active');
+			$('#boxInfo').css({
+				width: '900px',
+				'min-width': '700px',
+				padding: '15px 20px',
+				overflow: 'hidden',
+				background: '#fff',
+				'border-radius': '5px',
+				'font-size': '1.3rem',
+				'margin-top': '20px',
+				'margin-right': '20px',
+				'margin-bottom': '20px',
+				'-webkit-box-shadow': '0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)',
+				'-moz-box-shadow': '0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)',
+				'-ms-box-shadow': '0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)',
+				'-o-box-shadow': '0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)',
+				'box-shadow': '0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)'
+			});
+		});
+	};
 
-    let init = function () {
-        $("button").click(function () {
-            showJSON("./json/" + $(this).attr("json"));
-            $('button').removeClass('active');
-            $(this).addClass('active');
-            $("#boxInfo").css({
-                "width": "900px",
-                "min-width": "700px",
-                "padding": "15px 20px",
-                "overflow": "hidden",
-                "background": "#fff",
-                "border-radius": "5px",
-                "font-size": "1.3rem",
-                "margin-top": "20px",
-                "margin-right": "20px",
-                "margin-bottom": "20px",
-                "-webkit-box-shadow": "0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)",
-                "-moz-box-shadow": "0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)",
-                "-ms-box-shadow": "0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)",
-                "-o-box-shadow": "0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)",
-                "box-shadow": "0 3px 6px rgba(0, 0, 0, 0.32), 0 3px 6px rgba(0, 0, 0, 0.64)"
-            });
-        });
-    }
+	let showJSON = function(url) {
+		$.getJSON(url, function(data) {
+			let dinamicSelect = `<select name="" id="dinamicSelect" >`;
+			let devDivs = ``;
+			$.each(data, function(index, value) {
+				$('#boxInfo').css('text-align', 'justify');
+				if (value.aptitude && value.report) {
+					dinamicSelect += `<option value="${value.report}">${value.aptitude}</option>`;
+				} else if (value.itProfile && value.definition) {
+					dinamicSelect += `<option value="${value.definition}">${value.itProfile}</option>`;
+				} else if (value.type && value.skills) {
+					$('#boxInfo').css('text-align', 'center');
 
-    let showJSON = function (url) {
-        $.getJSON(url, function (data) {
-            let dinamicSelect = `<select name="" id="dinamicSelect" >`;
-            let devDivs = ``;
-            $.each(data, function (index, value) {
-                $("#boxInfo").css("text-align", "justify");
-                if (value.aptitude && value.report) {
-                    dinamicSelect += `<option value="${value.report}">${value.aptitude}</option>`;
-                } else if (value.itProfile && value.definition) {
-                    dinamicSelect += `<option value="${value.definition}">${value.itProfile}</option>`;
-                } else if (value.type && value.skills) {
-                    $("#boxInfo").css("text-align", "center");
-
-                    devDivs += 
-                    `<div class="container">
+					devDivs += `<div class="container">
                         <h4><b>${value.type}</b></h4>
                         <div class="skills">
                     `;
-                    for (let skill of value.skills) {
-                        devDivs += `<p>${skill}</p>`
-                    }
-                    devDivs += `</div>`;
-                }
-            });
+					for (let skill of value.skills) {
+						devDivs += `<p>${skill}</p>`;
+					}
+					devDivs += `</div>`;
+				}
+			});
 
-            dinamicSelect += `</select><p class='show'></p>`;
-            $("#boxInfo").html(dinamicSelect)
-            $(".show").html($("#dinamicSelect").val());
-            $("#dinamicSelect").change(function () {
-                $(".show").html($(this).val());
-            });
+			dinamicSelect += `</select><p class='show'></p>`;
+			$('#boxInfo').html(dinamicSelect);
+			$('.show').html($('#dinamicSelect').val());
+			$('#dinamicSelect').change(function() {
+				$('.show').html($(this).val());
+			});
 
-            if (devDivs !== '') {
-                devDivs += `</div>`;
-                $("#boxInfo").html(devDivs);
-                $(".skills").css("display", "none");
-                $(".container h4 ").click(function () {
-                    let parentElement = $(this).parent();
-                    let div = parentElement.children(".skills")
-                    div.slideToggle("slow");
-                });
-            }
-        });
-    }
-    $(init);
-
+			if (devDivs !== '') {
+				devDivs += `</div>`;
+				$('#boxInfo').html(devDivs);
+				$('.skills').css('display', 'none');
+				$('.container h4 ').click(function() {
+					let parentElement = $(this).parent();
+					let div = parentElement.children('.skills');
+					div.slideToggle('slow');
+				});
+			}
+		});
+	};
+	$(init);
 }
